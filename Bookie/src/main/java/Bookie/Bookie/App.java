6ea6c83f43e2f3f86181aa4bc4266c41cd07e1df
Alumno <Alumno@CC17>
2020-03-03 12:04:39 +0100

    First commit to set Bookie project

---
+++ Bookie/src/main/java/Bookie/Bookie/App.java
@@ -0,0 +1,13 @@
+package Bookie.Bookie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
