74a7943d606a94ff641567c5113a3ce3d4fa72a4
Carlos <carlos.ml5035@gmail.com>
2020-05-24 19:14:49 +0200

    Commit General-Pendiente 24/05/2020
    Commit inicial Pendiente

---
+++ MendoLopez-Carlos-general-Pendiente/src/main/java/MendoLopez_Carlos_general_Pendientes/MendoLopez_Carlos_general_Pendiente/App.java
@@ -0,0 +1,15 @@
+package MendoLopez_Carlos_general_Pendientes.MendoLopez_Carlos_general_Pendiente;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        
+    }
+}
